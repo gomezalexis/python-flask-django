6098baa4297f988df02d0124fdf37f2eb4275a3b
Alexis Gomez <gomez.alexis@gmail.com>
2017-09-20 19:26:25 -0500

    Finally Session Words

---
+++ django/session_words/apps/words/views.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from time import strftime,gmtime
+
+# Create your views here.
+def index(request):
+    if 'all_words' not in request.session:
+        request.session['all_words'] = []
+
+    print request.session['all_words']
+    return render(request, 'words/index.html')
+
+def add_word(request):
+    if request.method == "POST":
+        request.session['word'] = request.POST['word']
+        request.session['color'] = request.POST['color']
+        if not 'bold' in request.POST:  #request.POST['bold']
+            request.session['bold'] = "style=font-size:16px"
+
+        else:
+            request.session['bold'] = request.POST['bold']
+            
+        request.session['time'] = strftime("%b %d, %Y %I:%M %p")  
+        new_word = {
+            "word": request.POST['word'],
+            "color": request.POST['color'],
+            "bold": request.session['bold'],
+            "time": strftime("%b %d, %Y %I:%M %p")
+        }   
+        #attaching the words to the array initiated in index
+        request.session['all_words'].append(new_word)
+    print request.session['color']
+    return redirect('/')
+
+def clear(request):
+    request.session['all_words'] = []
+    return redirect('/')
--- django/survey_form/apps/survey/views.py
+++ django/survey_form/apps/survey/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 
-# Create your views here.
+
 def index(request):
     return render(request, 'survey/index.html')
 
